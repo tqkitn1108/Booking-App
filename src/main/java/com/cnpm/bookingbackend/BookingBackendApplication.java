856eb9e10e7780a37408cee5b5b18b9b7df8dae4
tqkitn1108 <quangkienta3@gmail.com>
2023-10-23 11:36:37 +0700

    create backend base

---
+++ src/main/java/com/cnpm/bookingbackend/BookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cnpm.bookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cnpm/bookingbackend/BookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnpm.bookingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

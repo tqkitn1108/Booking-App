ef4f068dd1bf774c429bb7bed256f6c2b7d05686
tqkitn1108 <quangkienta3@gmail.com>
2023-11-09 22:29:13 +0700

    push first task

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-import './App.css';
-import Footer from './components/footer/Footer';
-import Email from './components/email/Email';
-function App() {
-  return (
-    <div className="App">
-      <Email />
-      <Footer />
-    </div>
-    
-  );
-=======
 import React from "react";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Login from './components/SecurePage/Login'
 import Signup from './components/SecurePage/Signup'
 import SecurePage from './components/SecurePage/SecurePage'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from "./components/footer/Footer";
+import Email from "./components/email/Email";
 
 function App(){
   return (
@@ -25,10 +14,11 @@ function App(){
         <Route path ='/' element={<SecurePage />} ></Route>  
         <Route path ='/Login' element={<Login />} ></Route>
         <Route path ='/signup' element={<Signup />} ></Route> 
+        <Route path ='/footer' element={<Footer />} ></Route> 
+        <Route path ='/email' element={<Email />} ></Route> 
       </Routes>
    </BrowserRouter>
   )
->>>>>>> 32ccdf1f5e2e24fcad61fd0b8dcdbed15c16bc2c
 }
 
 export default App

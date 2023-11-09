322c086058bdb4c493ee4af007fee9029ec569bf
hauvietnam <haunguyenhuu7@gmail.com>
2023-11-09 22:24:21 +0700

    demo list search

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-import './App.css';
-import Footer from './components/footer/Footer';
-import Email from './components/email/Email';
-function App() {
-  return (
-    <div className="App">
-      <Email />
-      <Footer />
-    </div>
-    
-  );
-=======
 import React from "react";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Login from './components/SecurePage/Login'
 import Signup from './components/SecurePage/Signup'
 import SecurePage from './components/SecurePage/SecurePage'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import List from "./pages/list/List";
 
 function App(){
   return (
@@ -25,10 +13,10 @@ function App(){
         <Route path ='/' element={<SecurePage />} ></Route>  
         <Route path ='/Login' element={<Login />} ></Route>
         <Route path ='/signup' element={<Signup />} ></Route> 
+        <Route path ='/list' element = {<List />}></Route>
       </Routes>
    </BrowserRouter>
   )
->>>>>>> 32ccdf1f5e2e24fcad61fd0b8dcdbed15c16bc2c
 }
 
 export default App

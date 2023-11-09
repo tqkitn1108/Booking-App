0bfa37dabca223072e9ecd19f2698e881ea50c84
nguyenha-meiii <nguyenthuhat1k60@gmail.com>
2023-11-09 22:31:05 +0700

    :)))

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import './App.css';
 import Footer from './components/footer/Footer';
 import Email from './components/email/Email';
@@ -10,25 +10,6 @@ function App() {
     </div>
     
   );
-=======
-import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Login from './components/SecurePage/Login'
-import Signup from './components/SecurePage/Signup'
-import SecurePage from './components/SecurePage/SecurePage'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-function App(){
-  return (
-   <BrowserRouter>
-      <Routes>
-        <Route path ='/' element={<SecurePage />} ></Route>  
-        <Route path ='/Login' element={<Login />} ></Route>
-        <Route path ='/signup' element={<Signup />} ></Route> 
-      </Routes>
-   </BrowserRouter>
-  )
->>>>>>> 32ccdf1f5e2e24fcad61fd0b8dcdbed15c16bc2c
 }
 
 export default App

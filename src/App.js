41115d4d16795549af76f3c2de3ef624ac9da2a8
tqkitn1108 <quangkienta3@gmail.com>
2023-11-14 09:20:21 +0700

    check

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './components/SecurePage/Login'
-import Signup from './components/SecurePage/Signup'
-import SecurePage from './components/SecurePage/SecurePage'
-import Footer from './components/footer/Footer'
-import Email from './components/email/Email'
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Login from './components/reservation-page/Login';
+import Signup from './components/reservation-page/Signup';
+import ReservationPge from './components/reservation-page/ReservationPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Email from './components/email/Email';
+import Footer from './components/footer/Footer';
 import List from './pages/list/List';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<SecurePage />} ></Route>
+        <Route path='/' element={<ReservationPge />} ></Route>
         <Route path='/Login' element={<Login />} ></Route>
         <Route path='/signup' element={<Signup />} ></Route>
         <Route path='/footer' element={<Footer />} ></Route>
         <Route path='/email' element={<Email />} ></Route>
         <Route path='/list' element={<List />} ></Route>
+        {/* <Route path='/header' element={<Header />} ></Route>
+        <Route path='/navbar' element={<Navbar />} ></Route> */}
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/hotel/Hotel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+
+const Hotel = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Header type="list"/>
+        </div>
+    )
+}
+
+export default Hotel

0aa8709c9ca520d7ba6eb54beff494ef06f093a3
nguyenha-meiii <nguyenthuhat1k60@gmail.com>
2023-11-14 21:55:39 +0700

    update email + footer + favdes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Signup from './components/SecurePage/Signup'
 import SecurePage from './components/SecurePage/SecurePage'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import List from "./pages/list/List";
+import FavoriteDestinations from "./components/favoriteDestination/FavoriteDestination";
 
 function App(){
   return (
    <BrowserRouter>
       <Routes>
+        <Route path ='/favdes' element={<FavoriteDestinations />} ></Route> 
         <Route path ='/' element={<SecurePage />} ></Route>  
         <Route path ='/Login' element={<Login />} ></Route>
         <Route path ='/signup' element={<Signup />} ></Route> 

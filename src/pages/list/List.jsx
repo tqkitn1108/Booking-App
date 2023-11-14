edf49a05d495b2a8ad45eab6e43055c9ccc7e8ff
hauvietnam <haunguyenhuu7@gmail.com>
2023-11-14 23:12:02 +0700

    fix click label

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,14 +1,18 @@
-
+// import { Navbar } from "react-bootstrap"
 import { FormCheck } from "react-bootstrap";
 import "./list.css"
 import SearchItem from "./SearchItem"
 import {useState} from 'react';
 import Pagination from 'react-bootstrap/Pagination';
+// import Sliders from './Slider'
+// import Slider from 'bootstrap-slider'
 const List = () => {
+    <script src="https://code.jquery.com/jquery-latest.js"></script>
     
     return (
         <div>
             <div className="listContainer">
+                
                 <div className="listWrapper">
                     <div className="listSearch">
                         <h3 className="lsTitle">Xếp hạng chỗ nghỉ</h3>
@@ -20,100 +24,64 @@ const List = () => {
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="2sao"/>
+                                <label class="form-check-label" for="2sao">
                                     2 sao
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="3sao"/>
+                                <label class="form-check-label" for="3sao">
                                     3 sao
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="4sao"/>
+                                <label class="form-check-label" for="4sao">
                                     4 sao
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="5sao"/>
+                                <label class="form-check-label" for="5sao">
                                     5 sao
                                 </label>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="listWrapper">
-                    <div className="listSearch">
-                        <h3 className="lsTitle">Khu vực</h3>
-                        <div className="lsItem">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    Thanh Xuân
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    Đống Đa
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    Hoàn Kiếm
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    Tây Hồ
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
-                                    Cầu Giấy
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-            </div>
+                
                 <div className="listWrapper">
                     <div className="listSearch">
                         <h3 className="lsTitle">Tiện nghi</h3>
                         <div className="lsItem">
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="GiaDinh"/>
+                                <label class="form-check-label" for="GiaDinh">
                                     Phòng gia đình
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="HutThuoc"/>
+                                <label class="form-check-label" for="HutThuoc">
                                     Phòng hút thuốc
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="DoXe"/>
+                                <label class="form-check-label" for="DoXe">
                                     Chỗ đỗ xe
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="Wifi"/>
+                                <label class="form-check-label" for="Wifi">
                                     Wifi miễn phí
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="vatNuoi"/>
+                                <label class="form-check-label" for="vatNuoi">
                                     Cho phép mang theo vật nuôi
                                 </label>
                             </div>
@@ -125,32 +93,32 @@ const List = () => {
                         <h3 className="lsTitle">Tiện nghi phòng</h3>
                         <div className="lsItem">
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="nhinBien"/>
+                                <label class="form-check-label" for="nhinBien">
                                     Nhìn ra biển
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="tamRieng"/>
+                                <label class="form-check-label" for="tamRieng">
                                     Phòng tắm riêng
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="HoBoi"/>
+                                <label class="form-check-label" for="HoBoi">
                                     Hồ bơi riêng
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="BanCong"/>
+                                <label class="form-check-label" for="BanCong">
                                     Ban công
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <input class="form-check-input" type="checkbox" value="" id="DieuHoa"/>
+                                <label class="form-check-label" for="DieuHoa">
                                     Điều hòa không khí
                                 </label>
                             </div>
